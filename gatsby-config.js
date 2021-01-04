c58cf439e4829ecb3ccdc0eb2ea1ffdc6f775ac9
kingrayhan <rayhan095@gmail.com>
2021-01-04 18:53:27 +0600

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Gatsby`,
-    siteUrl: `https://www.gatsbyjs.com`,
-    description: `Blazing fast modern site generator for React`,
-  },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-postcss"],
-};
